1e20715de7ba89a3c1020f45b37a2385f764a98b
Siraya K <sirayamill@gmail.com>
2024-05-31 02:59:12 +0700

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
+# This function will create a matrix and cache the value of invert of the matrix
+# Using similar approach in README.md file, the function set the value of,
+# the matrix, then get the value, and after that it will set the invert, and
+# set the value
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL #default invert is null (not being calculated at the beginning)
+    set <- function(y) { 
+        x <<- y
+        inv <<- NULL #reset the inverse if matrix is reset
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
+#This function calculate the invert of the matrix that created by previous fx
+#If the invert of the matrix was calculated already, it will get the cached data
+#If not, it will calculate, and cache the value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv) 
+    }
+    data <- x$get()  # Get the matrix
+    inv <- solve(data, ...)  # Compute the inverse
+    x$setInverse(inv)  # Cache the inverse
+    inv  # Return the inverse
 }
